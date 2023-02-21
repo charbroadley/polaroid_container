7550b798a4234f6fd2d4145bd6232bbddeb2342a
Char Broadley <broadleychar@gmail.com>
2023-02-21 20:19:35 +0000

    first ideas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import drawing from "./images/drawing.jpg"
+
+const PolaroidContainer = styled.div`
+  display: grid;
+  grid-template-areas:
+    "image"
+    "text";
+  width: 450px;
+  height: 500px;
+  background-color: #fffefc;
+  border: 1.5px solid #3d3d3d;
+  border-radius: 5px;
+`
+// const ImageContainer = styled.div`
+//   grid-area: image;
+//   display: flex;
+//   justify-content: center;
+//   align-content: center;
+//   width: 390px;
+//   height: 390px;
+//   background-color: pink;
+//   border: 2px solid black;
+// `
+const Image = styled.img`
+grid-area: image;
+  width: 388px;
+  height: 388px;
+  margin: auto;
+`
+const TextContainer = styled.div`
+  grid-area: text;
+  margin: auto;
+  width: 390px;
+  height: 20px;
+  border: 2px solid #3d3d3d;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PolaroidContainer>
+    
+        <Image 
+          src={drawing}>
+        </Image>
+   
+      <TextContainer>
+
+      </TextContainer>
+    </PolaroidContainer>
   );
 }
 
